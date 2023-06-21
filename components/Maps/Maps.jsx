14f345996f2946d01604e261b1b21de426b5fa67
mimoun <zouhrimimoun21@gmail.com>
2023-06-21 12:31:09 -0700

    maps

---
+++ components/Maps/Maps.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import styles from '../Maps/maps.module.css'
+import { FaSearch } from 'react-icons/fa';
+import { GiPositionMarker } from 'react-icons/gi';
+import { LuList } from 'react-icons/lu';
+const containerStyle = {
+  width: '100%',
+  height: '400px',
+};
+
+const center = {
+  lat: 33.5731,
+  lng: -7.5898,
+};
+
+function Map() {
+  const handleInputChange = () => {
+    // Code pour traiter les changements de saisie
+  };
+
+  return (
+    <div className={styles.top}>
+      <div className={styles.top_seconde}>
+      <div className={styles.search_container}>
+          <div className={styles.top_container}>
+            <div className={styles.input_container}>
+              <input type="text" placeholder='School Name' className={styles.input_text}  onClick={handleInputChange}  />
+              <span className={styles.search_icon}><FaSearch /></span>
+            </div>
+            <div className={styles.input_container_two}>
+              <div>
+                <input type="text" placeholder='Find location' className={styles.input_text} />
+                <span className={styles.search_icon}><GiPositionMarker /></span>
+              </div>
+              <div className={styles.prox}>
+                <span className={styles.search_icons}> <LuList /> </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        
+      </div>
+      <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={14}
+        />
+      </LoadScript>
+      <div>
+      </div>
+    </div>
+  );
+}
+
+export default Map;
---
+++ pages/maps.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Maps from '../components/Maps/Maps'
+function maps() {
+  return (
+    <section>
+        <Maps />
+    </section>
+  )
+}
+
+export default maps
