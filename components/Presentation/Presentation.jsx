0f8db6e59a9e946535b8f95cdfe1c84d50bf4b65
mimoun <zouhrimimoun21@gmail.com>
2023-06-29 08:54:38 -0700

    desc

--- components/Presentation/Presentation.jsx
+++ components/Presentation/Presentation.jsx
@@ -4,39 +4,104 @@ import Image from 'next/image';
 import ico1 from '../../public/assets/icons/alt_route.svg'
 import ico2 from '../../public/assets/icons/call.svg'
 import ico3 from '../../public/assets/icons/share.svg'
+import icons4 from '../../public/assets/icons/Vector(4).svg'
+import icons5 from '../../public/assets/icons/Vector(5).svg'
+import icons6 from '../../public/assets/icons/Vector(6).svg'
+import icons7 from '../../public/assets/icons/Vector(6).svg'
 function Presentation() {
-  return (
-    <section className={styles.presentation}>
-        <div className={styles.pre_container}>
-            <div className={styles.pre_ico}>
-                <Image src={ico1}></Image>
+    return (
+        <section className={styles.presentation}>
+            <div className={styles.pre}>
+                <div className={styles.pre_container}>
+                    <div className={styles.pre_ico}>
+                        <Image src={ico1}></Image>
+                    </div>
+                    <div>
+                        <div className={styles.title}>347 BD ambassafeur ben Aicha, Casablanca </div>
+                        <div className={styles.ligne}></div>
+                    </div>
+                </div>
+                <div className={styles.pre_container}>
+                    <div className={styles.pre_ico}>
+                        <Image src={ico1}></Image>
+                    </div>
+                    <div>
+                        <div className={styles.title}>+212 565 33 212</div>
+                        <div className={styles.ligne}></div>
+                    </div>
+                </div>
+                <div className={styles.pre_container}>
+                    <div className={styles.pre_ico}>
+                        <Image src={ico1}></Image>
+                    </div>
+                    <div>
+                        <div className={styles.title}>www.gsr.ma </div>
+                        <div className={styles.ligne}></div>
+                    </div>
+                </div>
+                <div className={styles.pre_container}>
+                    <div className={styles.pre_ico}>
+                        <Image src={ico1}></Image>
+                    </div>
+                    <div>
+                        <div className={styles.title}>www.facebook.com/gsr </div>
+                        <div className={styles.ligne}></div>
+                    </div>
+                </div>
+                <div className={styles.pre_container}>
+                    <div className={styles.pre_ico}>
+                        <Image src={ico1}></Image>
+                    </div>
+                    <div>
+                        <div className={styles.title}>www.instagram.com/gsr </div>
+                        <div className={styles.ligne}></div>
+                    </div>
+                </div>
             </div>
-            <div>
-                <div className={styles.title}>347 BD ambassafeur ben Aicha, Casablanca </div>
-                <div className={styles.ligne}></div>
+            <div className={styles.description}>
+                <div className={styles.desc}>
+                    <div className={styles.titre}>Description</div>
+                    <div className={styles.ligne}></div>
+                </div>
+                <div className={styles.desc_content}>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. <br />
+                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
+                </div>
             </div>
-        </div>
-        <div className={styles.pre_container}>
-            <div className={styles.pre_ico}>
-                <Image src={ico1}></Image>
+            <div className={styles.cycle}>
+                <div className={styles.cy}>
+                    <div className={styles.titre}>Cycle</div>
+                    <div className={styles.ligne}></div>
+                </div>
+                <div className={styles.icons}>
+                    <div className={styles.icon} >
+                        <div className={styles.image_container}>
+                            <Image src={icons4} width={20} height={20} />
+                        </div>
+                        <div className={styles.cycle}>Maternelle</div>
+                    </div>
+                    <div className={styles.icon}>
+                        <div className={styles.image_container}>
+                            <Image src={icons5} width={20} height={20} />
+                        </div>
+                        <div className={styles.cycle}>Primaire</div>
+                    </div>
+                    <div className={styles.icon}>
+                        <div className={styles.image_container}>
+                            <Image src={icons6} width={20} height={20} />
+                        </div>
+                        <div className={styles.cycle}>Collège</div>
+                    </div>
+                    <div className={styles.icon}>
+                        <div className={styles.image_container}>
+                            <Image src={icons7} width={20} height={20} />
+                        </div>
+                        <div className={styles.cycle}>lycee</div>
+                    </div>
+                </div>
             </div>
-            <div>
-                <div className={styles.title}>347 BD ambassafeur ben Aicha, Casablanca </div>
-                <div className={styles.ligne}></div>
-            </div>
-        </div>
-        <div className={styles.pre_container}>
-            <div className={styles.pre_ico}>
-                <Image src={ico1}></Image>
-            </div>
-            <div>
-                <div className={styles.title}>347 BD ambassafeur ben Aicha, Casablanca </div>
-                <div className={styles.ligne}></div>
-            </div>
-        </div>
-
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Presentation
